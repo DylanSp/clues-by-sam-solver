c6b3a7e9dcc5f6a23111cfcddde56dd09f44113d
Dylan Sprague <ithilanor@gmail.com>
2025-09-20 00:30:53 +0000

    Install z3, set up demo program

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from z3 import *
+
+
 def main():
-    print("Hello from clues-by-sam-solver!")
+    x = Int('x')
+    y = Int('y')
+    s = Solver()
+    s.add(x > 2, y < 10, x + 2*y == 7)
+    print(s.check())
+    print(s.model())
 
 
 if __name__ == "__main__":

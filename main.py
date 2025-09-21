28ac986b349a9293a888ac8be94862c96cf8ad10
Dylan Sprague <ithilanor@gmail.com>
2025-09-21 14:54:41 +0000

    Write example of finding multiple solutions with z3

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from z3 import *
+from z3 import Int, Solver, sat, Or
 
 
 def main():
     x = Int('x')
     y = Int('y')
     s = Solver()
-    s.add(x > 2, y < 10, x + 2*y == 7)
-    print(s.check())
-    print(s.model())
+    s.add(x >= 0)
+    s.add(y >= 0)
+    s.add(x + y == 3)
+
+    while s.check() == sat:
+        print(s.model())
+        s.add(Or(x != s.model()[x], y != s.model()[y]))
 
 
 if __name__ == "__main__":

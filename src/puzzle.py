e8c88ed6573f7ff5776e9ca50c8cd867cd376db7
Dylan Sprague <dylan.richard.sprague@gmail.com>
2026-04-02 20:55:53 +0000

    Split PuzzleSuspect into separate file to avoid cyclic imports

--- src/puzzle.py
+++ src/puzzle.py
@@ -2,9 +2,10 @@ from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Any, Optional, Set
 
-from z3 import And, Bool, BoolRef, If, Not, Or, Solver, Sum, sat, unsat
+from z3 import And, Bool, If, Not, Or, Solver, Sum, sat, unsat
 
 from models import Column, Direction, Parity, PuzzleInput, Verdict
+from puzzle_suspect import PuzzleSuspect
 
 NUM_ROWS = 5
 NUM_COLS = 4
@@ -22,78 +23,6 @@ def parse_number_str(input: str) -> int:
         return int(input)
     except ValueError:
         return word_to_int[input]
-
-
-# all the data on a suspect that the Puzzle engine needs, including location, neighbors, and a Z3 BoolRef for tracking deduced verdict
-@dataclass
-class PuzzleSuspect:
-    name: str
-    profession: str
-    row: int
-    column: Column
-    neighbors: Set["PuzzleSuspect"]
-    all_other_suspects: Set["PuzzleSuspect"]
-    is_innocent: BoolRef  # Z3 expression - true iff suspect is innocent
-
-    # Names are unique and immutable, so can be used for equality testing; can't use default hash because Sets aren't hashable
-    def __hash__(self) -> int:
-        return hash(self.name)
-
-    def neighbor_to(self, direction: Direction) -> Optional["PuzzleSuspect"]:
-        for neighbor in self.neighbors:
-            match direction:
-                case Direction.ABOVE:
-                    if neighbor.column == self.column and neighbor.row == self.row - 1:
-                        return neighbor
-                case Direction.BELOW:
-                    if neighbor.column == self.column and neighbor.row == self.row + 1:
-                        return neighbor
-                case Direction.LEFT:
-                    if neighbor.row == self.row and int(neighbor.column) == int(self.column) - 1:
-                        return neighbor
-                case Direction.RIGHT:
-                    if neighbor.row == self.row and int(neighbor.column) == int(self.column) + 1:
-                        return neighbor
-        return None
-
-    def suspects_to(self, direction: Direction) -> Set["PuzzleSuspect"]:
-        match direction:
-            case Direction.ABOVE:
-                return set(
-                    [
-                        suspect
-                        for suspect in self.all_other_suspects
-                        if suspect.column == self.column and suspect.row < self.row
-                    ]
-                )
-            case Direction.BELOW:
-                return set(
-                    [
-                        suspect
-                        for suspect in self.all_other_suspects
-                        if suspect.column == self.column and suspect.row > self.row
-                    ]
-                )
-            case Direction.LEFT:
-                return set(
-                    [
-                        suspect
-                        for suspect in self.all_other_suspects
-                        if suspect.row == self.row and int(suspect.column) < int(self.column)
-                    ]
-                )
-            case Direction.RIGHT:
-                return set(
-                    [
-                        suspect
-                        for suspect in self.all_other_suspects
-                        if suspect.row == self.row and int(suspect.column) > int(self.column)
-                    ]
-                )
-
-    # Fast __repr__ method to allow debugging without needing to calculate repr for Z3 values
-    def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} at {hex(id(self))}"
 
 
 @dataclass
@@ -523,7 +452,8 @@ class Puzzle:
                 ]
                 | [
                     num_suspects,
-                    ("innocents" | "criminals") as verdict_str,  # TODO - can this be "innocent"/"criminal"?
+                    # TODO - can this be "innocent"/"criminal"?
+                    ("innocents" | "criminals") as verdict_str,
                     "neighboring",
                     suspect_with_neighbors,
                     "are",
@@ -532,7 +462,8 @@ class Puzzle:
                 ]
                 | [
                     num_suspects,
-                    ("innocents" | "criminals") as verdict_str,  # TODO - can this be "innocent"/"criminal"?
+                    # TODO - can this be "innocent"/"criminal"?
+                    ("innocents" | "criminals") as verdict_str,
                     "neighboring",
                     suspect_with_neighbors,
                     "are",
---
+++ src/puzzle_suspect.py
@@ -0,0 +1,78 @@
+from dataclasses import dataclass
+from typing import Optional, Set
+
+from z3 import BoolRef
+
+from models import Column, Direction
+
+
+# all the data on a suspect that the Puzzle engine needs, including location, neighbors, and a Z3 BoolRef for tracking deduced verdict
+@dataclass
+class PuzzleSuspect:
+    name: str
+    profession: str
+    row: int
+    column: Column
+    neighbors: Set["PuzzleSuspect"]
+    all_other_suspects: Set["PuzzleSuspect"]
+    is_innocent: BoolRef  # Z3 expression - true iff suspect is innocent
+
+    # Names are unique and immutable, so can be used for equality testing; can't use default hash because Sets aren't hashable
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def neighbor_to(self, direction: Direction) -> Optional["PuzzleSuspect"]:
+        for neighbor in self.neighbors:
+            match direction:
+                case Direction.ABOVE:
+                    if neighbor.column == self.column and neighbor.row == self.row - 1:
+                        return neighbor
+                case Direction.BELOW:
+                    if neighbor.column == self.column and neighbor.row == self.row + 1:
+                        return neighbor
+                case Direction.LEFT:
+                    if neighbor.row == self.row and int(neighbor.column) == int(self.column) - 1:
+                        return neighbor
+                case Direction.RIGHT:
+                    if neighbor.row == self.row and int(neighbor.column) == int(self.column) + 1:
+                        return neighbor
+        return None
+
+    def suspects_to(self, direction: Direction) -> Set["PuzzleSuspect"]:
+        match direction:
+            case Direction.ABOVE:
+                return set(
+                    [
+                        suspect
+                        for suspect in self.all_other_suspects
+                        if suspect.column == self.column and suspect.row < self.row
+                    ]
+                )
+            case Direction.BELOW:
+                return set(
+                    [
+                        suspect
+                        for suspect in self.all_other_suspects
+                        if suspect.column == self.column and suspect.row > self.row
+                    ]
+                )
+            case Direction.LEFT:
+                return set(
+                    [
+                        suspect
+                        for suspect in self.all_other_suspects
+                        if suspect.row == self.row and int(suspect.column) < int(self.column)
+                    ]
+                )
+            case Direction.RIGHT:
+                return set(
+                    [
+                        suspect
+                        for suspect in self.all_other_suspects
+                        if suspect.row == self.row and int(suspect.column) > int(self.column)
+                    ]
+                )
+
+    # Fast __repr__ method to allow debugging without needing to calculate repr for Z3 values
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} at {hex(id(self))}"
